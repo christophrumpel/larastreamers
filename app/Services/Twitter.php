6c46c3aa8bb26b51743ad0761f4a2fd63856eaca
freek <freek@spatie.be>
2021-05-15 14:35:10 +0200

    wip

---
+++ app/Services/Twitter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+class Twitter
+{
+    protected TwitterOAuth $twitter;
+
+    public function __construct(TwitterOAuth $twitter)
+    {
+        $this->twitter = $twitter;
+    }
+
+    public function tweet(string $status)
+    {
+        /*
+        if (! app()->environment('production')) {
+            return;
+        }
+        */
+
+        return (array)$this->twitter->post('statuses/update', compact('status'));
+    }
+}
