7fa551254ae13d30b93edd33bd0d172dc708d639
Christoph Rumpel <c.rumpel@kabsi.at>
2021-05-15 06:21:32 +0200

    Remove register test and add unit example test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Tests\Unit;
+
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test **/
+    public function exampleTest(): void
+    {
+    	$this->assertTrue(true);
+    }
+}
