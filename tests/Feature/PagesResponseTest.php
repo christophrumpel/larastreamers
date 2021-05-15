fcd95fdfc69a8f640c49001d1d55618133152d4a
freek <freek@spatie.be>
2021-05-15 13:56:20 +0200

    wip

--- tests/Feature/PagesResponseTest.php
+++ tests/Feature/PagesResponseTest.php
@@ -2,18 +2,27 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class PagesResponseTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase;
+
+    /** @test */
     public function it_shows_successful_response_for_home_page(): void
     {
+        $this->withoutExceptionHandling();
+
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_can_show_the_feed(): void
+    {
+        $response = $this->get('/feed');
 
         $response->assertStatus(200);
     }
