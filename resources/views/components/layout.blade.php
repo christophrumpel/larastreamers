1b5a68f733516de81e25dd49ef84bacc89e2f402
Sebastian Hilger <hilger@kraenk.de>
2021-05-15 10:19:55 +0200

    bring back meta viewport tag for responsiveness

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Larastreamers</title>
     <meta name="description" content="Larastreamers is an overview of who is streaming next in the Laravel world.">
