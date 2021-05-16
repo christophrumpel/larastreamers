58f8357680f831242ed04858d74804f23457f3fd
abenerd <me@abenet.email>
2021-05-16 09:26:06 +0200

    Simplify return types and fix method return.

--- app/Http/Livewire/ImportYoutubeLiveStream.php
+++ app/Http/Livewire/ImportYoutubeLiveStream.php
@@ -5,8 +5,6 @@ namespace App\Http\Livewire;
 use Alaouy\Youtube\Facades\Youtube;
 use App\Models\Stream;
 use Carbon\Carbon;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
@@ -14,19 +12,21 @@ class ImportYoutubeLiveStream extends Component
 {
     public $youtubeId;
 
-    public function render(): Factory|View|Application
+    public function render(): View
     {
         return view('livewire.import-youtube-live-stream');
     }
 
-    public function importStream()
+    public function importStream(): void
     {
         $video = Youtube::getVideoInfo($this->youtubeId, ['snippet', 'liveStreamingDetails']);
         if(!isset($video->snippet->liveBroadcastContent) || $video->snippet->liveBroadcastContent !== 'upcoming') {
-            return $this->addError('stream', 'This video is not an upcoming stream');
+            $this->addError('stream', 'This video is not an upcoming stream');
+
+            return;
         }
 
-        Stream::updateOrCreate(['youtube_id' => $video->id],[
+        Stream::updateOrCreate(['youtube_id' => $video->id], [
             'youtube_id' => $video->id,
             'channel_title' => $video->snippet->channelTitle,
             'title' => $video->snippet->title,
