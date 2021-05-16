cd5ed80a1149c77ae7c8fd89dc75d089315ef243
Sebastian Hilger <hilger@kraenk.de>
2021-05-16 14:29:16 +0200

    add alternate png icon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,8 @@
 
     <title>Larastreamers</title>
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📺</text></svg>">
+    <link rel="alternate icon" href="/images/favicon-tv.png">
+    
     <meta name="description" content="Larastreamers is an overview of who is streaming next in the Laravel world.">
     <meta name="author" content="Christoph Rumpel">
 
