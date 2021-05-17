b724c67f349015765479cc6804c5f88979839eba
Samuel Nitsche <mail@samynitsche.de>
2021-05-17 14:29:16 +0200

    Fix image aspect ratio on safari

--- resources/views/pages/partials/stream.blade.php
+++ resources/views/pages/partials/stream.blade.php
@@ -1,8 +1,10 @@
 <div class="bg-blue-100 rounded-md overflow-hidden">
     <a class="block flex flex-col md:flex-row" target="_blank"
        href="{{ $stream->link() }}">
-        <img class="w-full md:w-96 md:border-r-8 md:border-gray-800" src="{{ $stream->thumbnail_url }}" width="400"
-             alt="Youtube Live Stream Preview image"/>
+        <div>
+            <img class="w-full md:w-96 md:border-r-8 md:border-gray-800" src="{{ $stream->thumbnail_url }}" width="400"
+                 alt="Youtube Live Stream Preview image"/>
+        </div>
         <div class="md:border-l-8 md:border-gray-800 px-8 py-8 md:py-4">
             @ray($stream->scheduled_start_time)
             <x-local-time class="block text-2xl mb-2 text-gray-600" :date="$stream->scheduled_start_time" />
