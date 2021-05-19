a46ffa928ba83f2f4cb08a3b57b9bea9cbc817c1
Tom Witkowski <dev.gummibeer@gmail.com>
2021-05-19 11:57:53 +0200

    add some basic properties to calendar limit calendar entries to last year and future

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Stream;
+use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Spatie\IcalendarGenerator\Components\Calendar;
@@ -13,11 +14,15 @@ class CalendarController extends Controller
     {
         $calendar = Calendar::create()
             ->name('Larastreamers')
-            ->description('There is no better way to learn than by watching other developers code live. Find out who is streaming next in the Laravel world.');
+            ->description('There is no better way to learn than by watching other developers code live. Find out who is streaming next in the Laravel world.')
+            ->refreshInterval(CarbonInterval::hour()->totalMinutes)
+            ->productIdentifier('larastreamers.com');
 
-        Stream::query()->each(fn(Stream $stream) => $calendar->event(
-            $stream->toCalendarItem()
-        ));
+        Stream::query()
+            ->where('scheduled_start_time', '>=', now()->subYear()->startOfYear())
+            ->each(fn(Stream $stream) => $calendar->event(
+                $stream->toCalendarItem()
+            ));
 
         return response($calendar->get())
             ->header('Content-Type', 'text/calendar');
