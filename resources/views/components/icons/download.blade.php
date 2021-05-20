a05f64103a89a6e1267fd493882d9c725d706046
Samuel Nitsche <mail@samynitsche.de>
2021-05-20 13:51:55 +0200

    Extract icons to blade components

---
+++ resources/views/components/icons/download.blade.php
@@ -0,0 +1,12 @@
+<svg xmlns="http://www.w3.org/2000/svg"
+     class="w-6 h-6 text-gray-300"
+     fill="none"
+     viewBox="0 0 24 24"
+     stroke="currentColor"
+>
+    <path stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+    />
+</svg>
