76cb0a01d175e478a6baa858918e736f809d306a
Christoph Rumpel <c.rumpel@kabsi.at>
2021-06-16 15:34:05 +0200

    Add default value for channel lang code

--- database/migrations/2021_06_16_143518_add_language_code_to_channels.php
+++ database/migrations/2021_06_16_143518_add_language_code_to_channels.php
@@ -9,7 +9,7 @@ class AddLanguageCodeToChannels extends Migration
     public function up(): void
     {
         Schema::table('channels', function (Blueprint $table) {
-            $table->string('language_code')->after('platform_id');
+            $table->string('language_code')->after('platform_id')->default('en');
         });
     }
 
