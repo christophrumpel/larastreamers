5e83cea14daf68d49d5ad042ff03fb07623a5dcd
freek <freek@spatie.be>
2021-06-28 08:06:06 +0200

    wip

--- resources/views/mail/approved.blade.php
+++ resources/views/mail/approved.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 Hi,
 
-Your stream [{{ $stream->title }}]({{ $stream->url() }}) was approved. You can now view it on [on our homepage]({{ route('home') }})
+Your stream [{{ $stream->title }}]({{ $stream->url() }}) was approved. You can now view it on [our homepage]({{ route('home') }})
 
 Thanks,
 
