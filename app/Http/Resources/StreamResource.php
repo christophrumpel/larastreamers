d5be9efe9d9cf4d53683488ed11d1e8988a0dfcb
Steve McDougall <juststevemcd@gmail.com>
2021-07-12 08:21:26 +0100

    php_cs auto fix

--- app/Http/Resources/StreamResource.php
+++ app/Http/Resources/StreamResource.php
@@ -19,12 +19,12 @@ class StreamResource extends JsonResource
                 'starts' => [
                     'human' => $this->scheduled_start_time->diffForHumans(),
                     'string' => $this->scheduled_start_time->toDateTimeString(),
-                    'formatted' => $this->scheduled_start_time->format('D d.m.Y')
+                    'formatted' => $this->scheduled_start_time->format('D d.m.Y'),
                 ],
                 'status' => $this->status,
                 'language_code' => $this->language_code,
                 'live' => $this->isLive(),
-            ]
+            ],
         ];
     }
 }
