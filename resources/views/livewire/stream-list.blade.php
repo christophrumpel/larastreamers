dded6cccdae2de609c29cff7a4d138d0ba492760
Sonny Spaan <info@spaanproductions.nl>
2021-07-15 10:29:12 +0200

    Update Design

--- resources/views/livewire/stream-list.blade.php
+++ resources/views/livewire/stream-list.blade.php
@@ -10,7 +10,7 @@
                             <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                         </svg>
                     </div>
-                    <input wire:model.debounce.300ms="search" type="search" name="search" id="search" class="text-gray-600 focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-9 sm:text-sm border-gray-300 rounded-md" placeholder="Search">
+                    <input wire:model.debounce.300ms="search" type="search" name="search" id="search" class="focus:ring-red-400 focus:border-red-400 block w-full pl-9 shadow-sm sm:text-sm border-gray-300 rounded-md text-gray-600" placeholder="Search">
                 </div>
             </div>
         </div>
@@ -39,7 +39,7 @@
         <div>
             <div class="w-full max-w-6xl px-4 mx-auto sm:px-6 md:px-8">
                 <h2 class="text-center text-3xl font-bold tracking-tight text-white md:text-4xl">
-                    No results found.
+                    No streams found.
                 </h2>
             </div>
         </div>
