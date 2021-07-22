bc296ecf379820d3427fdd5bf7bb4bd32e214555
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-07-22 15:56:30 +0200

    Remove YouTube fake data which we do not need.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -46,7 +46,6 @@ abstract class TestCase extends BaseTestCase
                     'etag' => 'Pzp1Mw-n3p5jKJvaUEpd8vRWcwk',
                     'id' => 'UCdtd5QYBx9MUVXHm7qgEpxA',
                     'snippet' => [
-                        'channelId' => 'UCNlUCA4VORBx8X-h-rXvXEg',
                         'title' => 'Christoph Rumpel',
                         'description' => "Hi, I'm Christoph Rumpel, a web developer, and teacher from Vienna. For the last six years, I have been working as a backend developer using PHP and Laravel daily. Since 2018 I'm operating on my own as a freelancer, consultant, and teacher. Next, to my first ebook about building chatbots in PHP, I'm also the author of Laravel Core Adventures; a video series that teaches how Laravel works under the hood.\n\nI love coding, teaching, surfing, bouldering, and his Nintendo Switch.\n",
                         'customUrl' => 'christophrumpel',
