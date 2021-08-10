40bd0cea23b8349be4c0996963ab16e16bcbdf54
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-08-10 22:09:05 +0200

    Fix failing test

--- resources/views/pages/partials/stream.blade.php
+++ resources/views/pages/partials/stream.blade.php
@@ -1,10 +1,10 @@
-<li class="relative flex flex-col md:flex-row relative md:-ml-24">
-    <a class="w-1/2 flex-none overflow-hidden rounded md:-mr-12 z-0 px-6 md:px-0 mb-2 md:mb-0"
+<li class="grid lg:grid-cols-[1fr,2fr] lg:gap-4">
+    <a class="relative transition focus:ring-4 focus:ring-red-400 focus:outline-none rounded-t-xl lg:rounded-xl"
        title="Open on YouTube"
        target="_blank"
        href="{{ $stream->url() }}">
 
-        @if ($stream->isLive())
+        @if (!$isArchive && $stream->isLive())
             <div class="absolute top-2.5 right-2.5 z-10">
                 <div
                     class="flex px-2 py-1 space-x-2 bg-gray-900 rounded bg-opacity-80 backdrop-filter backdrop-blur-xl backdrop-saturate-150 place-items-center">
@@ -17,14 +17,21 @@
             </div>
         @endif
 
-        <figure class="flex items-center overflow-auto rounded-t-xl lg:rounded-xl aspect-w-16 aspect-h-9">
-            <img class="flex-none overflow-hidden rounded md:-mr-12 z-0"
+        <figure class="overflow-auto rounded-t-xl lg:rounded-xl aspect-w-16 aspect-h-9">
+            <img class="object-cover w-full h-full"
                  src="{{ $stream->thumbnail_url }}"
                  alt="Video thumbnail"/>
         </figure>
     </a>
-    <article
-        class="md:pl-24 px-6 flex flex-col justify-between items-start space-y-4 md:space-y-2 bg-gray-600 rounded-b-xl lg:rounded-xl">
+
+    <article class="flex flex-col items-start p-6 space-y-2 bg-gray-600 rounded-b-xl lg:rounded-xl">
+        <div class="flex items-center">
+            <x-local-time class="font-bold tracking-tight text-red-400" :date="$stream->actual_start_time ?? $stream->scheduled_start_time"/>
+            @if ($stream->language->shouldRender())
+                <span class="block mx-2 text-gray-500">&bull;</span>
+                <span class="font-semibold tracking-wider uppercase text-white">{{ $stream->language->name }}</span>
+            @endif
+        </div>
 
         @if($stream->duration)
             <div class="flex items-center space-x-2">
@@ -42,36 +49,30 @@
                 </a>
             </h3>
 
-            <p class="text-base text-gray flex items-center">
-                <x-icons.icon-user class="w-4 h-4 mr-2 inline text-gray fill-current stroke-current"/>
+            <p class="text-lg font-medium text-gray-300">
                 {{ $stream->channel_title }}
             </p>
-            <p class="text-base text-gray flex items-center">
-                <x-icons.icon-time class="w-4 h-4 mr-2 inline text-gray fill-current stroke-current"/>
-                <x-local-time class=""
-                              :date="$date = $stream->actual_start_time ?? $stream->scheduled_start_time"
-                              :format="$date->isToday() ? 'HH:mm (z)' : 'YYYY-MM-DD HH:mm (z)'"/>
-            </p>
-            @if ($stream->language->shouldRender())
-                <p class="text-base text-gray flex items-center">
-                    <x-icons.world class="w-4 h-4 mr-2 inline text-gray fill-current stroke-current"/>
-                    <span class="">{{ $stream->language->name }}</span>
-                </p>
-            @endif
         </header>
 
-        <ul class="flex flex-wrap gap-3 md:gap-6">
-            <li>
-                <x-stream-button link="{{ $stream->toWebcalLink() }}" name="Add to calendar">
-                    <x-icons.calendar/>
-                </x-stream-button>
-            </li>
+        @if (!$isArchive)
+            <ul class="flex flex-wrap gap-6">
+                <li>
+                    <a href="{{ $stream->toWebcalLink() }}"
+                       class="inline-flex items-center space-x-2 transition hover:text-gray-300">
+                        <x-icons.calendar/>
+                        <span class="text-sm font-medium">Add to calendar</span>
+                    </a>
+                </li>
 
-            <li>
-                <x-stream-button link="{{ route('calendar.ics.stream', $stream) }}" name="Download .ics file">
-                    <x-icons.download/>
-                </x-stream-button>
-            </li>
-        </ul>
+                <li>
+                    <a href="{{ route('calendar.ics.stream', $stream) }}"
+                       class="inline-flex items-center space-x-2 transition hover:text-gray-300">
+                        <x-icons.download/>
+
+                        <span class="text-sm font-medium">Download .ics file</span>
+                    </a>
+                </li>
+            </ul>
+        @endif
     </article>
 </li>
