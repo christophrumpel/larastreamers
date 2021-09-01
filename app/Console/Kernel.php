00e5acbf8a94b9ad538defa69d56e131c4d6043a
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-09-01 20:13:29 +0200

    Refactor weekly summary and handle no streams given

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,7 +7,7 @@ use App\Console\Commands\CheckIfUpcomingStreamsAreLiveCommand;
 use App\Console\Commands\ImportChannelStreamsCommand;
 use App\Console\Commands\TweetAboutLiveStreamsCommand;
 use App\Console\Commands\TweetAboutUpcomingStreamsCommand;
-use App\Console\Commands\TweetWeeklySummary;
+use App\Console\Commands\TweetAboutWeeklySummaryCommand;
 use App\Console\Commands\UpdateLiveAndFinishedStreamsCommand;
 use App\Console\Commands\UpdateUpcomingStreamsCommand;
 use Illuminate\Console\Scheduling\Schedule;
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
         TweetAboutLiveStreamsCommand::class,
         UpdateUpcomingStreamsCommand::class,
         ImportChannelStreamsCommand::class,
-        TweetWeeklySummary::class,
+        TweetAboutWeeklySummaryCommand::class,
     ];
 
     /**
---
+++ tests/Feature/Commands/TweetAboutWeeklySummaryCommandTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature\Commands;
+
+use App\Console\Commands\TweetAboutWeeklySummaryCommand;
+use App\Models\Stream;
+use Illuminate\Support\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TweetAboutWeeklySummaryCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_tweets_weekly_summary(): void
+    {
+    	// Arrange
+        $startOfLastWeek = Carbon::today()->subWeek()->startOfWeek();
+        $endOfLastWeek = Carbon::today()->subWeek()->endOfWeek()->endOfDay();
+
+        Stream::factory()
+            ->approved()
+            ->finished()
+            ->create(['actual_start_time' => $startOfLastWeek]);
+
+        Stream::factory()
+            ->approved()
+            ->finished()
+            ->create(['actual_start_time' => $endOfLastWeek]);
+
+        // Act
+        $this->artisan(TweetAboutWeeklySummaryCommand::class);
+
+    	// Assert
+        $this->twitterFake->assertTweetWasSent();
+        $this->twitterFake->assertLastTweetMessageWas('There were 2 streams last week. Thanks to all the viewers 🙏🏻.');
+    }
+
+    /** @test */
+    public function it_does_not_tweet_weekly_summary_when_no_streams_given(): void
+    {
+        // Arrange
+        $beforeLastWeek = Carbon::today()->subWeek()->startOfWeek()->subDay();
+        $afterLastWeek = Carbon::today();
+
+        Stream::factory()
+            ->approved()
+            ->finished()
+            ->create(['actual_start_time' => $beforeLastWeek]);
+
+        Stream::factory()
+            ->approved()
+            ->finished()
+            ->create(['actual_start_time' => $afterLastWeek]);
+
+        // Act
+        $this->artisan(TweetAboutWeeklySummaryCommand::class)
+            ->expectsOutput('There were no streams last week.');
+
+    	// Assert
+        $this->twitterFake->assertNoTweetsWereSent();
+    }
+}
