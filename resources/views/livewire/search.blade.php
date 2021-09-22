84cabd070a5e32d0bf9355864ab0912f7a2a37b5
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-09-22 08:22:34 +0200

    Typo

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-row rounded-lg overflow-hidden shadow-xl">
         <label for="search" class="sr-only">Search</label>
         <input id="search" class="w-80 text-gray-dark px-8 py-4 placeholder-gray-light focus:border-red focus:ring-red" placeholder="Laravel..." role="searchbox"
-               aria-placeholder="A streams's title"/>
+               aria-placeholder="A stream's title"/>
         <div class="flex items-center justify-center bg-red text-white py-2 px-4">
             <x-icons.search class="w-4 h-4 fill-current stroke-current"/>
         </div>
