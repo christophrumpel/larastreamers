f04e6a4be30f1a8ed36b0bd6fa9245085f2450e6
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-10-05 16:38:19 +0200

    Remove meta duplicates

--- resources/views/pages/partials/meta.blade.php
+++ resources/views/pages/partials/meta.blade.php
@@ -22,7 +22,3 @@
 <meta name="twitter:card" content="summary_large_image"/>
 <meta name="twitter:site" content="@larastreamers"/>
 <meta name="twitter:creator" content="@christophrumpel"/>
-<meta property="og:url" content="{{ url('') }}"/>
-<meta property="og:title" content="{{ $title }}"/>
-<meta property="og:description" content="{{ $description }}"/>
-<meta property="og:image" content="{{ asset('images/larastreamers_social.png') }}"/>
