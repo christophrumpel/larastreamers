0e0ccc6dab382283e5dd44541109ab60c49a4a60
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-10-05 14:58:22 +0200

    Run prod assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
     theme: {
         extend: {
             fontFamily: {
