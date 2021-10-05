d5ad00de655b04c503961e69a50f246b14876d20
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-10-05 16:36:25 +0200

    Update meta and add partial

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -16,29 +16,7 @@
     <link rel="alternate icon"
           href="{{ asset('images/favicon-tv.png') }}">
 
-    <meta name="description"
-          content="{{ $description }}">
-
-    <meta name="author"
-          content="Christoph Rumpel">
-
-    <meta property="og:url"
-          content="{{ url('') }}"/>
-
-    <meta property="og:type"
-          content="website"/>
-
-    <meta property="og:title"
-          content="{{ $title }}"/>
-
-    <meta property="og:description"
-          content="{{ $description }}"/>
-
-    <meta property="og:image"
-          content="{{ asset('images/larastreamers_social.png') }}"/>
-
-    <meta name="twitter:card" content="summary_large_image" />
-
+    @include('pages.partials.meta')
 
     <link href="{{ mix('css/app.css') }}"
           rel="stylesheet"/>
---
+++ resources/views/pages/partials/meta.blade.php
@@ -0,0 +1,28 @@
+<meta name="description"
+      content="{{ $description }}">
+
+<meta name="author"
+      content="Christoph Rumpel">
+
+<meta property="og:url"
+      content="{{ url('') }}"/>
+
+<meta property="og:type"
+      content="website"/>
+
+<meta property="og:title"
+      content="{{ $title }}"/>
+
+<meta property="og:description"
+      content="{{ $description }}"/>
+
+<meta property="og:image"
+      content="{{ asset('images/larastreamers_social.png') }}"/>
+
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:site" content="@larastreamers"/>
+<meta name="twitter:creator" content="@christophrumpel"/>
+<meta property="og:url" content="{{ url('') }}"/>
+<meta property="og:title" content="{{ $title }}"/>
+<meta property="og:description" content="{{ $description }}"/>
+<meta property="og:image" content="{{ asset('images/larastreamers_social.png') }}"/>
