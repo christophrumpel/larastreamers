7f71684b197c02daed37cb21bb06c03b6152adf6
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-10-21 11:42:36 +0200

    Add return type

--- app/Http/Controllers/Submission/ApproveStreamController.php
+++ app/Http/Controllers/Submission/ApproveStreamController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Submission;
 
 use App\Actions\Submission\ApproveStreamAction;
 use App\Models\Stream;
+use Illuminate\Contracts\View\View;
 
 class ApproveStreamController
 {
-    public function __invoke(Stream $stream, ApproveStreamAction $approveStream)
+    public function __invoke(Stream $stream, ApproveStreamAction $approveStream): View
     {
         $approveStream->handle($stream);
 
