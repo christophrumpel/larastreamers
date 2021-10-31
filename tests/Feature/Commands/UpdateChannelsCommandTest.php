21cd71445f1001cbd78507a44311030e56099c96
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-10-31 10:43:06 +0100

    Enhance command tests

--- tests/Feature/Commands/UpdateChannelsCommandTest.php
+++ tests/Feature/Commands/UpdateChannelsCommandTest.php
@@ -2,19 +2,24 @@
 
 namespace Tests\Feature\Commands;
 
-use App\Console\Commands\UpdateAllChannelDetails;
+use App\Console\Commands\UpdateChannelsCommand;
 use App\Facades\YouTube;
 use App\Models\Channel;
 use App\Services\YouTube\ChannelData;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
-class UpdateAllChannelDetailsCommandTest extends TestCase
+class UpdateChannelsCommandTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
-    public function it_update_channels(): void
+    public function it_updates_channels(): void
     {
         // Arrange
+        Http::fake();
         YouTube::partialMock()
             ->shouldReceive('channels')
             ->andReturn(collect([
@@ -29,12 +34,21 @@ class UpdateAllChannelDetailsCommandTest extends TestCase
                 ),
             ]));
 
-        Channel::factory()->create(['platform_id' => '1234']);
+        Channel::factory()
+            ->create([
+                'platform_id' => '1234',
+                'name' => 'My old channel name',
+                'description' => 'My old description',
+                'thumbnail_url' => 'my-old-thumbnail-url',
+                'on_platform_since' => Carbon::now()->subYears(1),
+                'country' => 'AT',
+            ]);
 
         // Act
-        $this->artisan(UpdateAllChannelDetails::class);
+        $this->artisan(UpdateChannelsCommand::class);
 
         // Assert
+        Http::assertNothingSent();
         $this->assertDatabaseCount(Channel::class, 1);
         $this->assertDatabaseHas(Channel::class, [
             'name' => 'My new test channel',
@@ -43,5 +57,17 @@ class UpdateAllChannelDetailsCommandTest extends TestCase
             'on_platform_since' => Carbon::now()->subYear(2),
             'country' => 'US',
         ]);
+    }
+
+    /** @test */
+    public function it_does_not_update_channels_if_no_channels_given(): void
+    {
+        // Arrange
+        Http::fake();
+
+        // Act & Assert
+        $this->artisan(UpdateChannelsCommand::class)
+            ->expectsOutput('There are no channels to update.')
+            ->assertExitCode(0);
     }
 }
