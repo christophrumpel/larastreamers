afe2779f43816470c6067169941945fe1ba6ca94
abenettt <me@abenet.email>
2021-12-08 16:28:44 +0100

    Update dependencies

--- config/feed.php
+++ config/feed.php
@@ -6,12 +6,12 @@ return [
             /*
              * Here you can specify which class and method will return
              * the items that should appear in the feed. For example:
-             * 'App\Model@getAllFeedItems'
+             * [App\Model::class, 'getAllFeedItems']
              *
-             * You can also pass an argument to that method:
-             * ['App\Model@getAllFeedItems', 'argument']
+             * You can also pass an argument to that method.  Note that their key must be the name of the parameter:
+             * [App\Model::class, 'getAllFeedItems', 'parameterName' => 'argument']
              */
-            'items' => 'App\Models\Stream@getFeedItems',
+            'items' => [\App\Models\Stream::class, 'getFeedItems'],
 
             /*
              * The feed will be available on this url.
@@ -23,14 +23,33 @@ return [
             'language' => 'en-US',
 
             /*
+             * The image to display for the feed.  For Atom feeds, this is displayed as
+             * a banner/logo; for RSS and JSON feeds, it's displayed as an icon.
+             * An empty value omits the image attribute from the feed.
+             */
+            'image' => '',
+
+            /*
+             * The format of the feed.  Acceptable values are 'rss', 'atom', or 'json'.
+             */
+            'format' => 'atom',
+
+            /*
              * The view that will render the feed.
              */
             'view' => 'feed::atom',
 
             /*
-             * The type to be used in the <link> tag
+             * The mime type to be used in the <link> tag.  Set to an empty string to automatically
+             * determine the correct value.
              */
-            'type' => 'application/atom+xml',
+            'type' => '',
+
+            /*
+             * The content type for the feed response.  Set to an empty string to automatically
+             * determine the correct value.
+             */
+            'contentType' => '',
         ],
     ],
 ];
