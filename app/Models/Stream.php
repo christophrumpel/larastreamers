d4d75c17a87986e562358df853bee88306fc18c3
Ken V <kenverhaegen@gmail.com>
2021-12-23 12:17:58 +0100

    Add null coalesce to attribute getter for startForRobots

--- app/Models/Stream.php
+++ app/Models/Stream.php
@@ -284,6 +284,6 @@ class Stream extends Model implements Feedable
 
     public function getStartForRobotsAttribute(): string
     {
-        return $this->actual_start_time->toIso8601String() ?? $this->scheduled_start_time->toIso8601String();
+        return $this->actual_start_time?->toIso8601String() ?? $this->scheduled_start_time?->toIso8601String();
     }
 }
