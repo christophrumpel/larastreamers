631bf1d5fc2e39a807075893d93d31383a11c441
Ken V <kenverhaegen@gmail.com>
2021-12-23 11:50:03 +0100

    Add datetime attribute to start time element

--- resources/views/pages/partials/header/preview.blade.php
+++ resources/views/pages/partials/header/preview.blade.php
@@ -1,15 +1,15 @@
 @if($upcomingStream)
-    <div class="hidden md:block relative w-2/5">
+    <div class="relative hidden w-2/5 md:block">
         <a href="{{ $upcomingStream->url() }}">
 
             <div
-                class="z-10 absolute -top-4 left-8 bg-red rounded-xl font-bold text-xs text-white uppercase tracking-widest p-2">
+                class="absolute z-10 p-2 text-xs font-bold tracking-widest text-white uppercase -top-4 left-8 bg-red rounded-xl">
                 @if($upcomingStream->isLive())  Live @else Upcoming @endif
             </div>
-            <div class="rounded-xl overflow-hidden shadow-lg">
+            <div class="overflow-hidden shadow-lg rounded-xl">
 
 
-                <div class="bg-white flex flex-col">
+                <div class="flex flex-col bg-white">
                     <figure class="overflow-auto aspect-w-16 aspect-h-9">
                         <img class="object-cover w-full h-full"
                              src="{{ $upcomingStream->thumbnail_url }}"
@@ -17,14 +17,14 @@
                     </figure>
 
                     <div class="p-8">
-                        <h3 class="mb-4 font-bold text-xl">{{ $upcomingStream->title }}</h3>
-                        <p class="mb-2 text-base text-gray-dark flex items-center">
-                            <x-icons.icon-user class="w-4 h-4 mr-2 inline text-gray-dark fill-current stroke-current"/>
+                        <h3 class="mb-4 text-xl font-bold">{{ $upcomingStream->title }}</h3>
+                        <p class="flex items-center mb-2 text-base text-gray-dark">
+                            <x-icons.icon-user class="inline w-4 h-4 mr-2 fill-current stroke-current text-gray-dark"/>
                             {{ $upcomingStream->channel->name }}  @if ($upcomingStream->language?->shouldRender())
                                 ({{ $upcomingStream->language->name }}) @endif
-                        <p class="text-base text-gray-dark flex items-center">
-                            <x-icons.icon-time class="w-4 h-4 mr-2 inline text-gray-dark fill-current stroke-current"/>
-                            <time class="text-base text-gray-dark flex">
+                        <p class="flex items-center text-base text-gray-dark">
+                            <x-icons.icon-time class="inline w-4 h-4 mr-2 fill-current stroke-current text-gray-dark"/>
+                            <time datetime="{{ $upcomingStream->actual_start_time }}" class="flex text-base text-gray-dark">
                                {{ $upcomingStream->startForHumans }}
                             </time>
                         </p>
