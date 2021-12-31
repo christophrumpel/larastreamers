261925f2b157d1a57343d5b325628511c53a0614
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-12-31 18:55:28 +0100

    Revert "Use Http macro to avoid calling configuration in class (#153)" (#154)
    This reverts commit b060409bc2ebea15322f4dec3f2d2ba736b969f8.

--- app/Services/YouTubeClient.php
+++ app/Services/YouTubeClient.php
@@ -6,6 +6,7 @@ use App\Services\YouTube\ChannelData;
 use App\Services\YouTube\StreamData;
 use App\Services\YouTube\YouTubeException;
 use Carbon\Carbon;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 
@@ -20,29 +21,36 @@ class YouTubeClient
 
     public function channels(iterable|string $channelIds): Collection
     {
-        return Http::youtube('channels', [
-            'id' => collect($channelIds)->implode(','),
+        return collect($this->fetch('channels', [
+            'id' => is_string($channelIds) ? $channelIds : collect($channelIds)->implode(','),
             'part' => 'snippet',
-        ], 'items')->map(fn(array $item) => new ChannelData(
-            platformId: data_get($item, 'id'),
-            youTubeCustomUrl: data_get($item, 'snippet.customUrl', ''),
-            name: data_get($item, 'snippet.title'),
-            description: data_get($item, 'snippet.description', ''),
-            onPlatformSince: $this->toCarbon(data_get($item, 'snippet.publishedAt')),
-            thumbnailUrl: last(data_get($item, 'snippet.thumbnails'))['url'] ?? null,
-            country: data_get($item, 'snippet.country', ''),
-        ));
+        ], 'items'))
+            ->map(fn(array $item) => new ChannelData(
+                platformId: data_get($item, 'id'),
+                youTubeCustomUrl: data_get($item, 'snippet.customUrl', ''),
+                name: data_get($item, 'snippet.title'),
+                description: data_get($item, 'snippet.description', ''),
+                onPlatformSince: $this->toCarbon(data_get($item, 'snippet.publishedAt')),
+                thumbnailUrl: last(data_get($item, 'snippet.thumbnails'))['url'] ?? null,
+                country: data_get($item, 'snippet.country', ''),
+            ));
     }
 
     public function upcomingStreams(string $channelId): Collection
     {
-        return Http::youtube('search', [
+        $videoIds = $this->fetch('search', [
             'channelId' => $channelId,
             'eventType' => 'upcoming',
             'type' => 'video',
             'part' => 'id',
             'maxResults' => 25,
-        ], 'items.*.id.videoId')->whenNotEmpty(fn(Collection $videoIds) => $this->videos($videoIds));
+        ], 'items.*.id.videoId');
+
+        if (empty($videoIds)) {
+            return collect();
+        }
+
+        return $this->videos($videoIds);
     }
 
     public function video(string $id): StreamData
@@ -54,10 +62,10 @@ class YouTubeClient
 
     public function videos(iterable|string $videoIds): Collection
     {
-        return Http::youtube('videos', [
-            'id' => collect($videoIds)->implode(','),
+        return collect($this->fetch('videos', [
+            'id' => is_string($videoIds) ? $videoIds : collect($videoIds)->implode(','),
             'part' => 'snippet,statistics,liveStreamingDetails',
-        ], 'items')
+        ], 'items'))
             ->map(fn(array $youTubeVideoDetails) => new StreamData(
                 videoId: data_get($youTubeVideoDetails, 'id'),
                 title: data_get($youTubeVideoDetails, 'snippet.title'),
@@ -77,6 +85,17 @@ class YouTubeClient
     {
         return $this->toCarbon(data_get($data, 'liveStreamingDetails.scheduledStartTime', now()))
             ?? $this->toCarbon(data_get($data, 'snippet.publishedAt', now()));
+    }
+
+    protected function fetch(string $url, array $params = [], string $key = null): array
+    {
+        return Http::asJson()
+            ->baseUrl('https://youtube.googleapis.com/youtube/v3/')
+            ->get($url, array_merge($params, [
+                'key' => config('services.youtube.key'),
+            ]))
+            ->onError(fn(Response $response) => throw YouTubeException::general($response->status(), $response->body()))
+            ->json($key, []);
     }
 
     protected function toCarbon(?string $string): ?Carbon
