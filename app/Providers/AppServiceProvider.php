664354c922f6fbb7a98108e8ef834e58389b283b
Abenet Tamiru <me@abenet.email>
2022-01-03 13:26:54 +0100

    Add HTTP macro for youtube api calls. (#155)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,27 @@
 
 namespace App\Providers;
 
+use App\Services\YouTube\YouTubeException;
+use Illuminate\Http\Client\Factory;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function boot(): void
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+        Http::macro('youtube', function(string $url, array $params = [], string $key = null): Collection {
+            /** @var Factory $this */
+            return $this
+                ->asJson()
+                ->baseUrl('https://youtube.googleapis.com/youtube/v3/')
+                ->get($url, array_merge($params, [
+                    'key' => config('services.youtube.key'),
+                ]))
+                ->onError(fn(Response $response) => throw YouTubeException::general($response->status(), $response->body()))
+                ->collect($key);
+        });
     }
 }
