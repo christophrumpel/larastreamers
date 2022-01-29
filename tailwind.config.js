8c98b45cfeec16d3d72ea56d8174d4d10a10a590
paddockDev <81797562+paddockDev@users.noreply.github.com>
2022-01-29 11:39:18 +0100

    Upgrade TailwindCSS to version 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
+    content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
@@ -38,15 +38,6 @@ module.exports = {
             }
         }
 
-    },
-
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-            animation: ['hover', 'group-hover'],
-            translate: ['group-hover'],
-            responsive: ['group-hover']
-        },
     },
 
     plugins: [
