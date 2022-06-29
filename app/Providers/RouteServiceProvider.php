6aaee1fc04a648ed163792d091daf6850d5f1561
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-06-29 13:50:48 +0200

    Fix phpstan issues

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -60,7 +60,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function(Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(60)->by((string) $request->user()?->id ?: (string) $request->ip());
         });
     }
 }
