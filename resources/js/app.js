3b280ac0e01543ba5d2b03fdd9e6dbea83ed90fc
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-06-29 13:44:26 +0200

    Adapt imports for Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
-window.moment = require('moment');
-require('moment-timezone');
+import moment from 'moment'
+window.moment = moment
+import momentTimezone from 'moment-timezone'
+window.moment = momentTimezone
+
 
 import Alpine from 'alpinejs'
 import trap from '@alpinejs/focus'
