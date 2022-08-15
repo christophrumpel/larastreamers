11589ee62d57a81331a0831542a0fa3e68bb9a6b
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2022-08-15 01:34:50 +0100

    change order of query

--- app/Http/Livewire/StreamListArchive.php
+++ app/Http/Livewire/StreamListArchive.php
@@ -44,7 +44,7 @@ class StreamListArchive extends Component
             ->paginate(24);
 
         // @phpstan-ignore-next-line
-        $channels = Channel::has('approvedFinishedStreams')->orderBy('name')->select('id', 'name')->withCount('approvedFinishedStreams')->get()->keyBy('hashId');
+        $channels = Channel::has('approvedFinishedStreams')->select('id', 'name')->orderBy('name')->withCount('approvedFinishedStreams')->get()->keyBy('hashId');
 
         return view('livewire.stream-list-archive', [
             'streams' => $streams,
