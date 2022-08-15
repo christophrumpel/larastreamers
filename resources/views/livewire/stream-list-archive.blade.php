69f987ed1252137ec3e64668213af90446104e23
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2022-08-15 14:09:04 +0100

    swap to total

--- resources/views/livewire/stream-list-archive.blade.php
+++ resources/views/livewire/stream-list-archive.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-page-header title="Archive ({{ $streams->count() }})">
+    <x-page-header title="Archive ({{ $streams->total() }})">
         <x-search wire:model.debounce.300ms="search" :channels="$channels" />
     </x-page-header>
 
