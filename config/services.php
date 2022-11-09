864fc5bb6129aa0a62b2872f6e0578dfac0b1500
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-11-09 12:54:31 +0100

    Add twitch credentials

--- config/services.php
+++ config/services.php
@@ -31,6 +31,12 @@ return [
         'key' => env('YOUTUBE_API_KEY'),
     ],
 
+    'twitch' => [
+        'client_id' => env('TWITCH_CLIENT_ID'),
+        'client_secret' => env('TWITCH_CLIENT_SECRET'),
+        'token' => env('TWITCH_TOKEN'),
+    ],
+
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
