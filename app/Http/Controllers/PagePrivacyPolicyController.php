b25cd54b71e9d3e4f7549196d09e6f61af74f7b6
Christoph Rumpel <christoph@christoph-rumpel.com>
2023-03-17 13:44:01 +0100

    Add missing return type

--- app/Http/Controllers/PagePrivacyPolicyController.php
+++ app/Http/Controllers/PagePrivacyPolicyController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+
 class PagePrivacyPolicyController extends Controller
 {
-    public function __invoke()
+    public function __invoke(): View
     {
         return view('pages.privacy-policy');
     }
