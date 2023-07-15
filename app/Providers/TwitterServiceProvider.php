f2c4b9d2f0a5d154ee5bd16da88ca6b907772e73
Christoph Rumpel <christoph@christoph-rumpel.com>
2023-07-15 09:05:36 +0200

    User Twitter API v2

--- app/Providers/TwitterServiceProvider.php
+++ app/Providers/TwitterServiceProvider.php
@@ -14,12 +14,16 @@ class TwitterServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(TwitterOAuth::class, function(): TwitterOAuth {
-            return new TwitterOAuth(
+            $twitter = new TwitterOAuth(
                 (string) config('services.twitter.consumer_key'),
                 (string) config('services.twitter.consumer_secret'),
                 (string) config('services.twitter.access_token'),
                 (string) config('services.twitter.access_token_secret')
             );
+
+            $twitter->setApiVersion('2');
+
+            return $twitter;
         });
 
         $this->app->bind(TwitterInterface::class, function(Application $app) {
