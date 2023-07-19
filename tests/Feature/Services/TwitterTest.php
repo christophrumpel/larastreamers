37ef13a866cfc4b161964f12ee7a96d442fc1fdc
christophrumpel <christophrumpel@users.noreply.github.com>
2023-07-19 07:46:27 +0000

    Apply php-cs-fixer changes

--- tests/Feature/Services/TwitterTest.php
+++ tests/Feature/Services/TwitterTest.php
@@ -1,11 +1,9 @@
 <?php
 
-
-use App\Facades\Twitter;
 use App\Services\Twitter\OAuthTwitter;
 use App\Services\Twitter\TwitterException;
 
-it('throws exception when Twitter replies with error', function () {
+it('throws exception when Twitter replies with error', function() {
     // Act
     app(OAuthTwitter::class)->tweet('test');
 })->throws(TwitterException::class);
