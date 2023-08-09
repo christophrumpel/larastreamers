693ac2c87992876a6ce731133cabd6249b95a7e3
Christoph Rumpel <christoph@christoph-rumpel.com>
2023-08-09 09:02:32 +0200

    do not throw exception for 201 response code

--- app/Services/Twitter/OAuthTwitter.php
+++ app/Services/Twitter/OAuthTwitter.php
@@ -15,7 +15,7 @@ class OAuthTwitter implements TwitterInterface
     {
         $response = $this->twitter->post('tweets', compact('text'), true);
 
-        if ($this->twitter->getLastHttpCode() > 200) {
+        if ($this->twitter->getLastHttpCode() > 201) {
             throw TwitterException::general($this->twitter->getLastHttpCode(), json_encode($response) ?? $response->title);
         }
 
