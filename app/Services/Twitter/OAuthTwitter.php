703b32ffaac6226d1012587c9d788ef44842fdec
Christoph Rumpel <christoph@christoph-rumpel.com>
2024-03-19 16:16:36 +0100

    Fix phpstan issues

--- app/Services/Twitter/OAuthTwitter.php
+++ app/Services/Twitter/OAuthTwitter.php
@@ -8,17 +8,19 @@ class OAuthTwitter implements TwitterInterface
 {
     public function __construct(
         protected TwitterOAuth $twitter,
-    ) {
+    )
+    {
     }
 
     public function tweet(string $text): ?array
     {
-        $response = $this->twitter->post('tweets', compact('text'), true);
+        $response = (array)$this->twitter->post('tweets', compact('text'), true);
 
         if ($this->twitter->getLastHttpCode() > 201) {
-            throw TwitterException::general($this->twitter->getLastHttpCode(), json_encode($response) ?? $response->title);
+
+            throw TwitterException::general($this->twitter->getLastHttpCode(), $response['title']);
         }
 
-        return (array) $response;
+        return $response;
     }
 }
