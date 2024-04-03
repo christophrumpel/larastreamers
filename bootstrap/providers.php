c492d07e7c7fefc2351ecdb967c4d522692766c6
Shift <shift@laravelshift.com>
2024-04-03 18:20:26 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\JetstreamServiceProvider::class,
+    App\Providers\TwitterServiceProvider::class,
 ];
