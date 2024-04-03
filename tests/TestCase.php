588d6060ed95609635ae4f11856866b318c40d4c
Shift <shift@laravelshift.com>
2024-04-03 18:20:35 +0000

    Remove `CreatesApplication` testing trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, LazilyRefreshDatabase;
+    use LazilyRefreshDatabase;
 
     private string $originalYoutubeApiKey;
 
