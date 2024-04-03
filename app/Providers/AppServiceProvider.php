463556d1f316513eea4b46bce1f8636ddece378c
Shift <shift@laravelshift.com>
2024-04-03 18:20:26 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
+use Illuminate\Cache\RateLimiting\Limit;
 use App\Services\YouTube\YouTubeException;
 use Illuminate\Http\Client\Factory;
 use Illuminate\Http\Client\Response;
@@ -11,6 +15,15 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to the "home" route for your application.
+     *
+     * This is used by Laravel authentication to redirect users after login.
+     *
+     * @var string
+     */
+    public const HOME = '/dashboard';
+
     public function boot(): void
     {
         Http::macro('youtube', function(string $url, array $params = [], string $key = null): Collection {
@@ -24,5 +37,24 @@ class AppServiceProvider extends ServiceProvider
                 ->onError(fn (Response $response) => throw YouTubeException::general($response->status(), $response->body()))
                 ->collect($key);
         });
+
+        $this->bootRoute();
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    public function bootRoute(): void
+    {
+        RateLimiter::for('api', function(Request $request) {
+            return Limit::perMinute(60)->by((string) $request->user()?->id ?: (string) $request->ip());
+        });
+
+        
     }
 }
