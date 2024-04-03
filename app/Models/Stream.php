c584bbb8a5fc3a36dcf0f40b1496d5a5c174667e
Alexandru Manase <malexandru699@gmail.com>
2024-04-03 23:43:50 +0300

    Fix PHPStan errors (#307)
    * wip
    
    * fix phpstan errors

--- app/Models/Stream.php
+++ app/Models/Stream.php
@@ -19,6 +19,10 @@ use Spatie\Feed\FeedItem;
 use Spatie\IcalendarGenerator\Components\Event;
 use Vinkla\Hashids\Facades\Hashids;
 
+/**
+ * @property \Illuminate\Support\Carbon $scheduled_start_time
+ * @property \Illuminate\Support\Carbon|null $actual_start_time
+ */
 class Stream extends Model implements Feedable
 {
     use HasFactory;
